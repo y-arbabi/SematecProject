cd99c4c2385f3eb5d626bcf358cdc6ff1da99f2e
y-arbabi <y.arbabi@yahoo.com>
2019-02-06 12:45:10 +0330

    Month Design

--- app/src/main/java/ir/sematec/sematecproject/MainActivity.java
+++ app/src/main/java/ir/sematec/sematecproject/MainActivity.java
@@ -11,42 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MYAPPSTAG","onCreate Called");
+
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("MYAPPSTAG","onStart Called");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("MYAPPSTAG","onResume Called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("MYAPPSTAG","onPause Called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("MYAPPSTAG","onStop Called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("MYAPPSTAG","onDestroy Called");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d("MYAPPSTAG","onRestart Called");
-    }
 }
